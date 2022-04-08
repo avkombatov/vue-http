35bb60c9f6aff9eab23f916f2bad94f01d0140c6
kombat <avkombatov@yandex.ru>
2022-04-08 18:34:19 +0300

    добавление записи с помощью fetch

--- src/App.vue
+++ src/App.vue
@@ -7,24 +7,39 @@
         <label for="name">Введите имя</label>
         <input type="text" id="name" v-model.trim="name" />
       </div>
-      <button class="btn primary" :disabled="name.length === 0">Создать человека</button>
+      <button class="btn primary" :disabled="name.length === 0">
+        Создать человека
+      </button>
     </form>
   </div>
 </template>
 
 <script>
 export default {
-  data(){
-    return{
-      name: '',
-    }
+  data() {
+    return {
+      name: "",
+    };
   },
-  methods:{
-    createPerson() {
-      // https://vue-http-27323-default-rtdb.firebaseio.com/
+  methods: {
+    async createPerson() {
+      // https://vue-http-27323-default-rtdb.firebaseio.com/people.json
 
-    }
-  }
+     const response = await fetch("https://vue-http-27323-default-rtdb.firebaseio.com/people.json", {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json",
+        },
+        body: JSON.stringify({
+          firstname: this.name,
+        }),
+      });
+
+      const firebaseData = await response.json();
+      console.log(firebaseData);
+      this.name = ''
+    },
+  },
 };
 </script>
 
