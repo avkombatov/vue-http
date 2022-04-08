f2fdef1b780392b8569e0a44a5eceac9f78d8d23
kombat <avkombatov@yandex.ru>
2022-04-08 14:07:14 +0300

    first commit

---
+++ src/App.vue
@@ -0,0 +1,32 @@
+<template>
+  <div class="container">
+    <form action="" class="card" @submit.prevent="createPerson">
+      <h2>Работа с базой данных</h2>
+
+      <div class="form-control">
+        <label for="name">Введите имя</label>
+        <input type="text" id="name" v-model.trim="name" />
+      </div>
+      <button class="btn primary" :disabled="name.length === 0">Создать человека</button>
+    </form>
+  </div>
+</template>
+
+<script>
+export default {
+  data(){
+    return{
+      name: '',
+    }
+  },
+  methods:{
+    createPerson() {
+      // https://vue-http-27323-default-rtdb.firebaseio.com/
+
+    }
+  }
+};
+</script>
+
+<style>
+</style>
